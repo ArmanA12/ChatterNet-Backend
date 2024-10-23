edf15d9737ecc235ee1ffb232a53b64cc721d825
Arman Shekh <97970564+ArmanA12@users.noreply.github.com>
2024-10-24 01:00:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,6 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const session = require('express-session');
-const rateLimit = require("express-rate-limit");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
 
@@ -56,16 +55,33 @@ dotenv.config();
 // MONGODB CONNECTION
 connectDB();
 
-// Rate Limiter
-const limiter = rateLimit({
-    windowMs: 60 * 60 * 1000,
-    max: 500,
-    message: {
-        success: false,
-        message: "Too many requests from this IP, please try again after an hour",
-    }
+// REST OBJECT
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(session({
+    secret: process.env.JWT_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+// ROUTES
+app.use("/api/v1/auth", require("./routes/userRoute"));
+app.use("/api/v1/post", require("./routes/postRoute"));
+app.use("/api/v1/followunfollow", require("./routes/followunfollwoRoute"));
+
+app.get('', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "welcome back manin"
+    });
 });
 
+// Export the app as a serverless function
+module.exports = app;
 // REST OBJECT
 const app = express();
 
